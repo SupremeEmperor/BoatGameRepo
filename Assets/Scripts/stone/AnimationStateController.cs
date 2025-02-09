e44f40eefef465640a8faacb67b393b7a8e8d6c9
breaker <miller.stone7711@gmail.com>
2025-02-09 00:38:58 -0800

    Added Animation script to work with WASD on art Test.unity

--- Assets/Scripts/stone/AnimationStateController.cs
+++ Assets/Scripts/stone/AnimationStateController.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class AnimationStateController : MonoBehaviour
 {
+    Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        if (!Input.GetKey("w"))
+        {
+            anim.SetBool("isWalking", false);
+        }
     }
+
 }
